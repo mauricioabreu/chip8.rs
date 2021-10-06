c864367d0c095f2e8f110c1f7bfe6f68d6832565
Maurício Antunes <mauricio.abreua@gmail.com>
2021-10-06 09:05:21 -0300

    Draw pixel (XOR)

--- src/machine.rs
+++ src/machine.rs
@@ -1,5 +1,8 @@
+const DISPLAY_WIDTH: usize = 64;
+const DISPLAY_HEIGHT: usize = 64;
+
 pub struct Machine {
-    ram: [u8; 4096],
+    memory: [u8; 4096],
     display: [[bool; 32]; 64],
     pc: u16,
     i: u16,
@@ -9,7 +12,7 @@ pub struct Machine {
 impl Machine {
     pub fn new() -> Self {
         let mut machine = Self {
-            ram: [0; 4096],
+            memory: [0; 4096],
             display: [[false; 32]; 64],
             pc: 0x200,
             i: 0,
@@ -21,8 +24,8 @@ impl Machine {
 
     pub fn decode(self: &mut Machine) -> OpCode {
         let op_code = OpCode::from_hex(
-            self.ram[usize::from(self.pc)],
-            self.ram[usize::from(self.pc + 1)],
+            self.memory[usize::from(self.pc)],
+            self.memory[usize::from(self.pc + 1)],
         );
         self.pc += 2;
 
@@ -38,51 +41,59 @@ impl Machine {
                 self.pc = op_code.nnn;
             }
             0x6u8 => {
-                self.v[op_code.x] = op_code.nn;
+                self.v[op_code.x as usize] = op_code.nn;
             }
             0x7u8 => {
-                self.v[op_code.x] += op_code.nn;
+                self.v[op_code.x as usize] += op_code.nn;
             }
             0xAu8 => {
                 self.i = op_code.nnn;
             }
             0xDu8 => {
-                let vx = self.v[op_code.x];
-                let vy = self.v[op_code.y];
+                self.draw_on_display(self, op_code);
+            }
+        }
+    }
 
-                // handle wrapping
-                let start_x = vx % 64;
-                let start_y = vy % 32;
+    fn draw_on_display(self: &mut Machine, op_code: OpCode) {
+        /* Draw on display in the register using the vx, vy coordinates.
 
-                self.v[0xF] = false;
+        If the current pixel in the sprite row is on and the pixel
+        at coordinates X,Y on the screen is also on,
+        turn off the pixel (XOR).
+        */
+        let vx = self.v[op_code.x as usize];
+        let vy = self.v[op_code.y as usize];
 
-                let mut i = 0;
-                for row in 0..op_code.n {
-                    let y_coord = start_y + row;
+        // handle wrapping
+        let start_x = vx % DISPLAY_WIDTH;
+        let start_y = vy % DISPLAY_HEIGHT;
 
-                    // break when reaches the edge
-                    if y >= 32 {
-                        break;
-                    }
+        self.v[0xF] = false;
 
-                    let sprite = self.ram[self.i + i];
+        for row in 0..op_code.n {
+            let y_coord = start_y + row;
 
-                    for column in 0..8 {
-                        let x_coord = start_x + column;
-                        // break when reaches the edge
-                        if x_coord >= 64 {
-                            break;
-                        }
+            // break when reaches the edge
+            if y_coord >= 32 {
+                break;
+            }
 
-                        /*
-                        If the current pixel in the sprite row is on and the pixel
-                        at coordinates X,Y on the screen is also on,
-                        turn off the pixel and set VF to 1
-                        */
-                        let current_pixel = self.display[x][y];
-                    }
+            let sprite = self.memory[self.i + row as u16];
 
-                    i += 1;
+            for column in 0..8 {
+                let x_coord = start_x + column;
+                // break when reaches the edge
+                if x_coord >= 64 {
+                    break;
+                }
+
+                let current_pixel = self.display[x][y];
+                let new_pixel = (sprite >> (7 - column)) & 1;
+                self.display[x][y] = current_pixel ^ new_pixel;
+
+                if current_pixel != 0 && self.display[x][y] == 0 {
+                    self.v[0xF] = true;
                 }
             }
         }
