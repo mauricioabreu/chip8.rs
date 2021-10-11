2bd153c164ec266f6c7e98d91581f7447863650e
Maurício Antunes <mauricio.abreua@gmail.com>
2021-10-11 13:17:06 -0300

    Remove display usage for a while

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,7 @@
-use display::Display;
 use machine::Machine;
 use std::env;
 use std::fs::File;
 use std::io::Read;
-use std::{thread, time::Duration};
 
 mod display;
 mod machine;
@@ -17,13 +15,9 @@ fn main() {
     f.read_to_end(&mut data).expect("File not found...");
     let mut machine = Machine::new();
     machine.load_rom(data);
-    let sdl_context = sdl2::init().unwrap();
-    let mut display = Display::new(&sdl_context);
 
     loop {
         let op = machine.decode_op();
         machine.execute_op(op);
-        display.draw(machine.read_display());
-        thread::sleep(Duration::from_secs(1));
     }
 }
