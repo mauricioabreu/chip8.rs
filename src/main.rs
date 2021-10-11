01579aeab323bfe2977d3b37800ecd7161d7f59b
Maurício Antunes <mauricio.abreua@gmail.com>
2021-10-11 19:48:51 -0300

    Try to display on the real screen again

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ use machine::Machine;
 use std::env;
 use std::fs::File;
 use std::io::Read;
+use std::time::{Duration, Instant};
 
 mod display;
 mod machine;
@@ -15,9 +16,22 @@ fn main() {
     f.read_to_end(&mut data).expect("File not found...");
     let mut machine = Machine::new();
     machine.load_rom(data);
+    let sdl_context = sdl2::init().unwrap();
+    let mut display = display::Display::new(&sdl_context);
+
+    let mut last_op_time = Instant::now();
+    let mut last_display_time = Instant::now();
 
     loop {
-        let op = machine.decode_op();
-        machine.execute_op(op);
+        if Instant::now() - last_op_time > Duration::from_millis(2) {
+            let op = machine.decode_op();
+            machine.execute_op(op);
+            last_op_time = Instant::now();
+        }
+
+        if Instant::now() - last_display_time > Duration::from_millis(10) {
+            display.draw(machine.read_display());
+            last_display_time = Instant::now();
+        }
     }
 }
